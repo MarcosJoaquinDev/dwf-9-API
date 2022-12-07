17c73242be2ce692efe35cd0d7fedc2aaa576f27
Acuña <marcosjuako@hotmail.com>
2022-12-07 17:30:46 -0300

    move to nextjs

---
+++ pages/api/index.js
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req,res) {
+  res.status(200).json({
+    body: req.body,
+    query: req.query,
+    cookies: req.cookies,
+  });
+}
